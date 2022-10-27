00579aff2caa777b1b777e28325ec10b0e8789b5
DaminovSherzod <daminovsherzod2002@gmail.com>
2022-10-27 12:10:12 +0500

    create add product

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,57 @@
+from itertools import product
 from django.shortcuts import render
+from .models import Product
+from django.http import JsonResponse, HttpResponse
 # from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
     
     return render(request, 'home.html')
+
+def convert_to_json(product):
+    """
+    Convert a product to a JSON object
+    args:
+        product: a product object
+    returns:
+        a JSON object
+    """
+    product_json = {
+        'id': product.id,
+        'name': product.name,
+        'company': product.company,
+        'color': product.color,
+        'RAM': product.RAM,
+        'memory': product.memory,
+        'price': product.price,
+        'created_at': product.created_at,
+        'updated_at': product.updated_at,
+        'img_url': product.img_url,
+    }
+    return product_json
+
+def add_product(request):
+    """
+    Create a product
+    args:
+        request: the request object
+    return:
+        JsonResponse: the product
+    """
+    if request.method == 'POST':
+        product = Product.objects.create(
+            name=request.POST['name'],
+            company=request.POST['company'],
+            color=request.POST['color'],
+            RAM=request.POST['RAM'],
+            memory=request.POST['memory'],
+            price=request.POST['price'],
+            img_url=request.POST['img_url']
+        )
+        product_json = convert_to_json(product)
+        product.save()
+
+        return JsonResponse({'product' : product_json})
+
+    return JsonResponse({'product': {}})
--- myserver/urls.py
+++ myserver/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home
+from myapp.views import home, add_product
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home )
+    path('home/',home ),
+    path('add_product/',add_product)
+
 ]
